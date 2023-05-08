4c784bd121dfd6640dc82c06dfd5ed3ad9a05be4
Salih Cantekin <salihcantekin@gmail.com>
2023-05-08 14:26:37 +0100

    Add project files.

---
+++ SpansInCSharp.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+using BenchmarkDotNet.Running;
+using SpansInCSharp.Console;
+
+BenchmarkRunner.Run<StringVsSpanBenchmarks>();
+return;
+
+
+
+
+// String vs Span<T>
+
+string input = "This is an example sentence.";
+
+ReadOnlySpan<char> inputSpan = input.AsSpan();
+
+Span<char> a = new[] { 'A', 'B' };
+
+a = "salih".ToArray();
+a = "salh".ToCharArray();
+Span<char> c = a.Slice(0, 2);
+
+string name = "salih";
+
+var newName = name.Substring(0, 2);
+
+var firstTwo = "sa";
+
---
+++ SpansInCSharp.Console/StringVsSpanBenchmarks.cs
@@ -0,0 +1,62 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SpansInCSharp.Console;
+
+[MemoryDiagnoser]
+public class StringVsSpanBenchmarks
+{
+    private string input;
+    private readonly string searchTerm = "example";
+    private readonly string replaceTerm = "sample";
+    private readonly string startsWith = "This";
+
+    private ReadOnlySpan<char> InputSpan => input.AsSpan();
+    private ReadOnlySpan<char> SearchTermSpan => searchTerm.AsSpan();
+    private ReadOnlySpan<char> ReplaceTermSpan => replaceTerm.AsSpan();
+    private ReadOnlySpan<char> StartsWithSpan => startsWith.AsSpan();
+
+    [Params(1000)]
+    public int Iterations { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        input = string.Concat(Enumerable.Repeat("This is an example sentence. ", Iterations));
+    }
+
+    [Benchmark]
+    public string StringReplace() => input.Replace(searchTerm, replaceTerm);
+
+    [Benchmark]
+    public string SpanReplace()
+    {
+        return ReplaceSpan(InputSpan, SearchTermSpan, ReplaceTermSpan).ToString();
+    }
+
+    [Benchmark]
+    public string StringSubstring() => input[..20];
+
+    [Benchmark]
+    public ReadOnlySpan<char> SpanSlice() => InputSpan[..20];
+
+    [Benchmark]
+    public bool StringStartsWith() => input.StartsWith(startsWith);
+
+    [Benchmark]
+    public bool SpanStartsWith() => InputSpan.StartsWith(StartsWithSpan);
+
+    private static ReadOnlySpan<char> ReplaceSpan(ReadOnlySpan<char> source, ReadOnlySpan<char> oldValue, ReadOnlySpan<char> newValue)
+    {
+        int index = source.IndexOf(oldValue);
+        if (index == -1) return source;
+
+        char[] result = new char[source.Length + newValue.Length - oldValue.Length];
+        Span<char> resultSpan = result;
+
+        source.Slice(0, index).CopyTo(resultSpan);
+        newValue.CopyTo(resultSpan.Slice(index));
+        source.Slice(index + oldValue.Length).CopyTo(resultSpan.Slice(index + newValue.Length));
+
+        return resultSpan;
+    }
+}
